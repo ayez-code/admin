7a94669e44f075848e860b3196d2140c2aa513dd
ayez-code <sayrosales4@gmail.com>
2025-05-08 00:00:40 +0700

    AdminPanelController.java

--- Admin Panel/src/admin/AdminPanelController.java
+++ Admin Panel/src/admin/AdminPanelController.java
@@ -2,6 +2,9 @@ package admin;
 
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
 
 public class AdminPanelController {
 
@@ -10,10 +13,6 @@ public class AdminPanelController {
         System.out.println("Dashboard button clicked");
     }
 
-    @FXML
-    private void handleUsersClick(MouseEvent event) {
-        System.out.println("Users button clicked");
-    }
 
     @FXML
     private void handleRoomClick(MouseEvent event) {
@@ -25,6 +24,25 @@ public class AdminPanelController {
         System.out.println("Settings button clicked");
     }
 
+    @FXML
+    private AnchorPane
+    dashboardContent;
 
+    @FXML
+    private void handleUsersClick(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/UsersPanel.fxml")); // use '/' if it's at the root of resources
+            Parent usersContent = loader.load(); // no need to cast, just use Parent
+
+            dashboardContent.getChildren().setAll(usersContent); // replace inner content
+            AnchorPane.setTopAnchor(usersContent, 0.0);
+            AnchorPane.setBottomAnchor(usersContent, 0.0);
+            AnchorPane.setLeftAnchor(usersContent, 0.0);
+            AnchorPane.setRightAnchor(usersContent, 0.0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
+
