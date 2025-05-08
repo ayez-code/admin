17ae032c013499ada63f5a1f6c5c83c09f53d3fe
ayez-code <sayrosales4@gmail.com>
2025-05-08 23:17:32 +0700

    AdminPanelController.java

--- Admin Panel/src/admin/AdminPanelController.java
+++ Admin Panel/src/admin/AdminPanelController.java
@@ -1,17 +1,22 @@
 package admin;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.security.cert.PolicyNode;
 
 public class AdminPanelController {
 
-    @FXML
-    private void handleDashboardClick(MouseEvent event) {
-        System.out.println("Dashboard button clicked");
-    }
+    private Stage primaryStage;
+    private Scene scene;
+    private Parent root;
 
 
     @FXML
@@ -26,23 +31,51 @@ public class AdminPanelController {
 
     @FXML
     private AnchorPane
-    dashboardContent;
+            dashboardContent;
 
     @FXML
-    private void handleUsersClick(MouseEvent event) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/UsersPanel.fxml")); // use '/' if it's at the root of resources
-            Parent usersContent = loader.load(); // no need to cast, just use Parent
+    private AnchorPane usersContent;
 
-            dashboardContent.getChildren().setAll(usersContent); // replace inner content
-            AnchorPane.setTopAnchor(usersContent, 0.0);
-            AnchorPane.setBottomAnchor(usersContent, 0.0);
-            AnchorPane.setLeftAnchor(usersContent, 0.0);
-            AnchorPane.setRightAnchor(usersContent, 0.0);
+    @FXML
+    private BorderPane dashboardRoot;
+
+    @FXML
+    private AnchorPane adminContent;
+
+
+    @FXML
+    private void handleDashboardClick (MouseEvent event){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/DashboardPanel.fxml"));
+            Parent dashboardContent = loader.load();
+
+
+            adminContent.getChildren().setAll(dashboardContent);
+            AnchorPane.setTopAnchor(dashboardContent, 0.0);
+            AnchorPane.setBottomAnchor(dashboardContent, 0.0);
+            AnchorPane.setLeftAnchor(dashboardContent, 0.0);
+            AnchorPane.setRightAnchor(dashboardContent, 0.0);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-}
+        @FXML
+        private void handleUsersClick (MouseEvent event){
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/UsersPanel.fxml"));
+                Parent usersContent = loader.load();
+
+                adminContent.getChildren().setAll(usersContent);
+                AnchorPane.setTopAnchor(usersContent, 0.0);
+                AnchorPane.setBottomAnchor(usersContent, 0.0);
+                AnchorPane.setLeftAnchor(usersContent, 0.0);
+                AnchorPane.setRightAnchor(usersContent, 0.0);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 
